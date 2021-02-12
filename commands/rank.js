4042c3c130d778f1b6370c4b144ebfe88475b6c6
Guilherme Afonso <guilherme.asp@live.com>
2021-02-12 14:05:36 -0300

    Comment unfinished code

--- commands/rank.js
+++ commands/rank.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args) {
         message.channel.send("Todo mundo tem vida aparentemente");
     },
-    count(client, member, args) {
-        client.channels.cache.get(process.env.BOT_TESTING_CHANNEL_ID).send(`Tô de olho viu <@${member.user.id}>! 👀`)
-    }
+    // count(client, member, args) {
+    //     client.channels.cache.get(process.env.BOT_TESTING_CHANNEL_ID).send(`Tô de olho viu <@${member.user.id}>! 👀`)
+    // }
 }
