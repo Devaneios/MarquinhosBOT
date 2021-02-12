9a94aafba552cbd38b0e864b02cb03a7396c321e
Guilherme Afonso <guilherme.asp@live.com>
2021-02-12 14:04:23 -0300

    Fix and update horario with the new clock module

--- commands/horario.js
+++ commands/horario.js
@@ -1,30 +1,20 @@
 const player = require("./../utils/player");
+const clock = require("../utils/clock");
 module.exports = {
     name: "horario",
     description: "Te digo o horário. Apenas.",
     execute(message, args) {
-        let hoje = new Date();
-        let horas = hoje.getHours();
-        // To GMT -3 
-        horas -= 3;
-        if(horas < 0){
-            horas = 24 + horas;
-        }
+        
+        let recifeHourAndMinute = clock.getHoursAndMinutesWithTimeZone('pt-BR', 'America/Recife');
+        let recifeHour = clock.getHoursWithTimeZone('pt-BR', 'America/Recife');
         newUserChannel = message.member.voice.channel;
         // If its midnight, Marquinhos enter the voice channel and ANNOUNCES that it's OLEO DE MACACO TIME
-        if (horas == 0) {
+        if (recifeHour == "00") {
             filepath = "./resources/sounds/macaco.mp3";
             player.execute(message, filepath, newUserChannel);
         } else {
             // If its not midnight, Marquinhos send the time in the channel
-            minutos = hoje.getMinutes();
-            if (minutos < 10) {
-                minutos = `0${minutos}`;
-            }
-            if (horas < 10) {
-                horas = `0${horas}`;
-            }
-            message.channel.send(`Agora são ${horas}:${minutos}`);
+            message.channel.send(`Agora são ${recifeHourAndMinute}`);
         }
     },
 };
