7dca6350f6f3132e5adbb124f27e270b7d3aeb84
Tiago <tscs2@cin.ufpe.br>
2021-02-13 18:37:47 -0300

    testing only

--- commands/user.js
+++ commands/user.js
@@ -1,8 +1,20 @@
+const fetch = require('node-fetch');
+const manage = require("./../utils/management").manage;
+const dj = require("./../utils/dj").dj;
 module.exports = {
     name: "user",
     description: "admin only.",
     hide: true,
     async execute(message, args) {
         message.delete();
+        getLyrics("Gabrielle Aplin", "Dear Happy");
     },
 };
+
+async function getLyrics(artist, song){
+    let URL = `https://api.lyrics.ovh/v1/${encodeURI(artist)}/${encodeURI(song)}`;
+    
+    let response = await (fetch(URL, { method: 'get' }));
+    let dados = await response.json();
+    console.log(dados.lyrics);    
+}
