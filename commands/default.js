0d55e76418c734872b8f0de4d8bed0d5d9514eaa
Guilherme Afonso <guilherme.asp@live.com>
2021-07-11 05:07:57 -0300

    fix default command

--- commands/default.js
+++ commands/default.js
@@ -1,23 +1,24 @@
-const manager = require('../utils/management').manager;
+const manager = require("../utils/management").manager;
 const tmdbService = require("../services/tmdbService").tmdbService;
+const moment = require("moment");
 
 module.exports = {
-    name: "default",
-    description: "Default fallback message",
-    usage: "!default",
-    hide: true,
-    async execute(message, args) {
-        try {
-            let searchResult = await tmdbService.searchMovie("lalaland");
-            console.log(searchResult);
-            message.channel.send("Favor digitar um comando válido.");
-        } catch (error) {
-            message.reply("quebrei! :(");
-            if(manager.debug){
-                manager.debugChannel.send("```"+`${error.message}\n${error.stack}`+"```");
-            }else{
-                console.log(error);
-            }
-        }
-    },
+	name: "default",
+	description: "Default fallback message",
+	usage: "!default",
+	hide: true,
+	async execute(message, args) {
+		try {
+			message.channel.send("Favor digitar um comando válido.");
+		} catch (error) {
+			message.reply("quebrei! :(");
+			if (manager.debug) {
+				manager.debugChannel.send(
+					"```" + `${error.message}\n${error.stack}` + "```"
+				);
+			} else {
+				console.log(error);
+			}
+		}
+	},
 };
