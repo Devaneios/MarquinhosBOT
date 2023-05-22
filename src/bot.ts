e6dfc8e2316517f0ac1ebcb4f335d598fe841f58
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-05-22 01:29:21 -0300

    fix: load audio commands separately

--- src/bot.ts
+++ src/bot.ts
@@ -5,25 +5,41 @@ import {
 	REST,
 	Routes,
 	SlashCommandBuilder,
+	Message,
+	EmbedBuilder,
 } from "discord.js";
 import { BotEvent, Command, SlashCommand } from "./types";
 import { readdirSync } from "fs";
 import { join } from "path";
 import { logger } from "./utils/logger";
 import { safeExecute } from "./utils/errorHandling";
+import { playAudio, voiceChannelPresence } from "./utils/discord";
 
-const { Guilds, MessageContent, GuildMessages, GuildMembers } =
-	GatewayIntentBits;
+const {
+	Guilds,
+	MessageContent,
+	GuildMessages,
+	GuildMembers,
+	GuildVoiceStates,
+} = GatewayIntentBits;
 
 class Bot {
 	private client: Client;
 	private readonly slashCommandsDir = join(__dirname, "./slashCommands");
-	private readonly commandsDir = join(__dirname, "./commands");
+	private readonly commandsDir = join(__dirname, "./commands/");
+	private readonly audiosDir = join(__dirname, "./resources/sounds/");
 	private readonly eventsDir = join(__dirname, "./events");
+	private readonly slashCommands: SlashCommandBuilder[] = [];
 
 	constructor() {
 		this.client = new Client({
-			intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
+			intents: [
+				Guilds,
+				MessageContent,
+				GuildMessages,
+				GuildMembers,
+				GuildVoiceStates,
+			],
 		});
 		this.client.slashCommands = new Collection<string, SlashCommand>();
 		this.client.commands = new Collection<string, Command>();
@@ -31,15 +47,13 @@ class Bot {
 	}
 
 	loadSlashCommands() {
-		const slashCommands: SlashCommandBuilder[] = [];
-
 		readdirSync(this.slashCommandsDir).forEach((file) => {
 			try {
 				if (!file.endsWith(".js")) return;
 
 				const command: SlashCommand =
 					require(`${this.slashCommandsDir}/${file}`).default;
-				slashCommands.push(command.command);
+				this.slashCommands.push(command.command);
 				this.client.slashCommands.set(command.command.name, command);
 
 				logger.info(
@@ -50,8 +64,6 @@ class Bot {
 				logger.error(error);
 			}
 		});
-
-		this._sendSlashCommands(slashCommands);
 	}
 
 	loadTextCommands() {
@@ -64,6 +76,58 @@ class Bot {
 					require(`${this.commandsDir}/${file}`).default;
 				commands.push(command);
 				this.client.commands.set(command.name, command);
+				logger.info(`Successfully read command ${command.name}`);
+			} catch (error) {
+				logger.error(
+					`Error loading command ${file.replace(".js", "")}`
+				);
+				logger.error(error);
+			}
+		});
+	}
+
+	loadAudioCommands() {
+		readdirSync(this.audiosDir).forEach((file) => {
+			try {
+				if (!file.endsWith(".mp3")) return;
+				const command: Command = {
+					name: file.replace(".mp3", ""),
+					execute: (message: Message, args: string[]) => {
+						const channel = voiceChannelPresence(message);
+						playAudio(message, channel, file.replace(".mp3", ""));
+					},
+					cooldown: 10,
+					aliases: [],
+					permissions: [],
+				};
+
+				const slashCommand: SlashCommand = {
+					command: new SlashCommandBuilder()
+						.setName(file.replace(".mp3", ""))
+						.setDescription(`Playing ${file.replace(".mp3", "")}`),
+					execute: (interaction) => {
+						const channel = voiceChannelPresence(interaction);
+						playAudio(
+							interaction,
+							channel,
+							file.replace(".mp3", "")
+						);
+						interaction.reply({
+							embeds: [
+								new EmbedBuilder().setDescription(
+									`Playing ${file.replace(".mp3", "")}`
+								),
+							],
+						});
+					},
+					cooldown: 10,
+				};
+				this.slashCommands.push(slashCommand.command);
+				this.client.commands.set(command.name, command);
+				this.client.slashCommands.set(
+					slashCommand.command.name,
+					slashCommand
+				);
 				logger.info(`Successfully read command ${command.name}`);
 			} catch (error) {
 				logger.error(
@@ -93,11 +157,11 @@ class Bot {
 		this.client.login(process.env.TOKEN);
 	}
 
-	private _sendSlashCommands(slashCommands: SlashCommandBuilder[]) {
+	sendSlashCommands() {
 		const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 		rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-			body: slashCommands.map((command) => command.toJSON()),
+			body: this.slashCommands.map((command) => command.toJSON()),
 		})
 			.then((data: any) => {
 				if (!data.length) {
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ config();
 
 const marquinhos = new Bot();
 marquinhos.loadTextCommands();
+marquinhos.loadAudioCommands();
 marquinhos.loadSlashCommands();
+marquinhos.sendSlashCommands();
 marquinhos.loadEvents();
 marquinhos.start();
