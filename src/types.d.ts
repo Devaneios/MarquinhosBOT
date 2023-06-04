37b6e5931dc8b8dd3f1062bac284eba0808fd23d
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-06-03 23:43:46 -0300

    feat: add chat secreto

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,62 +1,71 @@
 import {
-	SlashCommandBuilder,
-	CommandInteraction,
-	Collection,
-	PermissionResolvable,
-	Message,
-	AutocompleteInteraction,
-} from "discord.js";
+  SlashCommandBuilder,
+  CommandInteraction,
+  Collection,
+  PermissionResolvable,
+  Message,
+  AutocompleteInteraction,
+} from 'discord.js';
+import { Subject } from 'rxjs';
 
 export interface SlashCommand {
-	command: SlashCommandBuilder | any;
-	execute: (interaction: CommandInteraction) => void;
-	autocomplete?: (interaction: AutocompleteInteraction) => void;
-	cooldown?: number; // in seconds
+  command: SlashCommandBuilder | any;
+  execute: (interaction: CommandInteraction) => void;
+  autocomplete?: (interaction: AutocompleteInteraction) => void;
+  cooldown?: number; // in seconds
 }
 
 export interface Command {
-	name: string;
-	execute: (message: Message, args: Array<string>) => void;
-	permissions: Array<PermissionResolvable>;
-	aliases: Array<string>;
-	cooldown?: number;
+  name: string;
+  execute: (message: Message, args: Array<string>) => void;
+  permissions: Array<PermissionResolvable>;
+  aliases: Array<string>;
+  cooldown?: number;
 }
 
 interface GuildOptions {
-	prefix: string;
+  prefix: string;
 }
 
 export interface IGuild {
-	guildID: string;
-	options: GuildOptions;
-	joinedAt: Date;
+  guildID: string;
+  options: GuildOptions;
+  joinedAt: Date;
 }
 
 export type GuildOption = keyof GuildOptions;
 export interface BotEvent {
-	name: string;
-	once?: boolean | false;
-	execute: (...args) => void;
+  name: string;
+  once?: boolean | false;
+  execute: (...args) => void;
+}
+
+export interface SecretChannelData {
+  channel: TextChannel;
+  startedAt: Date;
+  finishesAt: Date;
+  messages: Subject<Message>;
 }
 
 export type SafeAny = any;
 
-export type BotErrorLogLevel = "error" | "warn" | "info";
+export type BotErrorLogLevel = 'error' | 'warn' | 'info';
 
 declare global {
-	namespace NodeJS {
-		interface ProcessEnv {
-			TOKEN: string;
-			CLIENT_ID: string;
-			PREFIX: string;
-		}
-	}
+  namespace NodeJS {
+    interface ProcessEnv {
+      TOKEN: string;
+      CLIENT_ID: string;
+      PREFIX: string;
+    }
+  }
 }
 
-declare module "discord.js" {
-	export interface Client {
-		slashCommands: Collection<string, SlashCommand>;
-		commands: Collection<string, Command>;
-		cooldowns: Collection<string, number>;
-	}
+declare module 'discord.js' {
+  export interface Client {
+    slashCommands: Collection<string, SlashCommand>;
+    commands: Collection<string, Command>;
+    cooldowns: Collection<string, number>;
+    secretChannels: Collection<string, SecretChannelData>;
+  }
 }
