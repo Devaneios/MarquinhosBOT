6ef3a15dba1677dcdf70296ba7db6c80fd481287
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-06-04 00:00:53 -0300

    refactor: improve anom code style

--- src/commands/anom.ts
+++ src/commands/anom.ts
@@ -1,50 +1,77 @@
-import { Message, TextChannel } from "discord.js";
-import { Command } from "../types";
-import BotError from "../utils/botError";
-import { sendTimedMessage } from "../utils/discord";
+import {
+  Collection,
+  GuildBasedChannel,
+  Message,
+  TextChannel,
+} from 'discord.js';
+import { Command } from '../types';
+import { sendTimedMessage } from '../utils/discord';
+import BotError from '../utils/botError';
 
 const command: Command = {
-	name: "anom",
-	execute: (message: Message, args: string[]) => {
-		const parsedMessage = messageHandler(args);
-		const channelName = args[1];
-		if (!channelName) channelNotProvided(message);
-		const desiredChannel = message.guild?.channels.cache
-			.filter((channel) => channel.name === channelName)
-			.first();
-		if (!desiredChannel) {
-			channelNotFound(message, channelName);
-		}
-		message.delete();
-		(desiredChannel as TextChannel).send(`Alguém disse: ${parsedMessage}`);
-	},
-	cooldown: 10,
-	aliases: ["anonimo"],
-	permissions: [],
+  name: 'anom',
+  execute: (message: Message, args: string[]) => {
+    const incommingChannel = message.channel as TextChannel;
+    const channelName = args[1];
+    const channels = message.guild?.channels.cache;
+    if (channelNotProvided(channelName, incommingChannel)) {
+      throw new BotError('Channel not specified', message, 'warn');
+    }
+
+    const parsedMessage = messageHandler(args);
+
+    const desiredChannel = searchChannel(
+      channelName,
+      channels,
+      incommingChannel
+    );
+
+    if (desiredChannel) {
+      (desiredChannel as TextChannel).send(`Alguém disse: ${parsedMessage}`);
+    } else {
+      throw new BotError(`Channel ${channelName} not found`, message, 'warn');
+    }
+  },
+  cooldown: 10,
+  aliases: ['anonimo'],
+  permissions: [],
 };
 
 const messageHandler = (args: string[]) => {
-	return args.slice(2).join(" ");
+  return args.slice(2).join(' ');
 };
 
-const channelNotProvided = (message: Message) => {
-	message.delete();
-	sendTimedMessage(
-		"Você precisa especificar um canal!",
-		message.channel as TextChannel,
-		5000
-	);
-	throw new BotError("Channel not specified", message, "warn");
+const channelNotProvided = (
+  channelName: string,
+  incommingChanel: TextChannel
+): boolean => {
+  if (!channelName) {
+    sendTimedMessage(
+      'Você precisa especificar um canal!',
+      incommingChanel,
+      5000
+    );
+    return true;
+  }
+  return false;
 };
 
-const channelNotFound = (message: Message, channelName: string) => {
-	message.delete();
-	sendTimedMessage(
-		"Desculpe, não consegui achar esse canal! :(",
-		message.channel as TextChannel,
-		5000
-	);
-	throw new BotError(`Channel ${channelName} not found`, message, "warn");
+const searchChannel = (
+  channelName: string,
+  channels: Collection<string, GuildBasedChannel> | undefined,
+  incommingChannel: TextChannel
+) => {
+  if (!channels) return null;
+  const desiredChannel = channels
+    .filter((channel) => channel.name === channelName)
+    .first();
+  if (desiredChannel) return desiredChannel;
+  sendTimedMessage(
+    'Desculpe, não consegui achar esse canal! :(',
+    incommingChannel,
+    5000
+  );
+  return null;
 };
 
 export default command;
