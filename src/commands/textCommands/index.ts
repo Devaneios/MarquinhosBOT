d520d18f0b1a2ed7022cc3b245f2c43de9b34553
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-06-17 17:56:08 -0300

    chore: update secret chat file name

--- src/commands/textCommands/index.ts
+++ src/commands/textCommands/index.ts
@@ -1,4 +1,4 @@
 export { anom } from './anom';
 export { cavalo } from './cavalo';
 export { chaos } from './chaos';
-export { chatSecreto } from './chat-secreto';
+export { secretChat } from './secretChat';
