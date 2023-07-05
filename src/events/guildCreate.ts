6e694f3517441ca8179afcec421eb9aa0df02102
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-07-05 15:38:09 -0300

    feat: add guild to mongo after join

---
+++ src/events/guildCreate.ts
@@ -0,0 +1,20 @@
+import { Guild } from 'discord.js';
+import GuildModel from '../schemas/guild';
+import { BotEvent } from '../types';
+
+export const guildCreate: BotEvent = {
+  name: 'guildCreate',
+  execute: (guild: Guild) => {
+    let newGuild = new GuildModel({
+      guildID: guild.id,
+      options: {
+        prefix: process.env.PREFIX,
+        VIP_ROLE_NAME: null,
+        BASE_ROLE_NAME: null,
+        EXTERNAL_ROLE_NAME: null,
+      },
+      joinedAt: Date.now(),
+    });
+    newGuild.save();
+  },
+};
