6926218a8bec050c03b75c01db96ac81787fbcaf
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-07-05 15:41:32 -0300

    fix: remove allow member command

--- src/commands/textCommands/index.ts
+++ src/commands/textCommands/index.ts
@@ -3,5 +3,4 @@ export { cavalo } from './cavalo';
 export { chaos } from './chaos';
 export { secretChat } from './secretChat';
 export { disconnectAll } from './disconnectAll';
-export { allowMember } from './allowMember';
 export { clone } from './clone';
