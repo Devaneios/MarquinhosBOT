63ee8be8f80a58ead2276904a2b8d107e09af611
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-07-05 15:38:09 -0300

    feat: add guild to mongo after join

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,4 @@
 export { interactionCreate } from './interactionCreate';
 export { messageCreate } from './messageCreate';
 export { ready } from './ready';
+export { guildCreate } from './guildCreate';
