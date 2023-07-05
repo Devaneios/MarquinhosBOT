7a8026596632a97857d14fdae3bdda85d8ef9740
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-07-05 15:37:30 -0300

    feat: add untracked guilds

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
 import { Client } from 'discord.js';
 import { BotEvent } from '../types';
 import { logger } from '../utils/logger';
+import GuildModel from '../schemas/guild';
 
 export const ready: BotEvent = {
   name: 'ready',
@@ -8,5 +9,22 @@ export const ready: BotEvent = {
   execute: (client: Client) => {
     logger.info(`Logged in as ${client.user?.tag}`);
     logger.info(`Marquinhos™ is online!`);
+
+    const guilds = client.guilds.cache.map((guild) => guild.id);
+
+    guilds.forEach(async (guild) => {
+      if (await GuildModel.exists({ guildID: guild })) return;
+      let newGuild = new GuildModel({
+        guildID: guild,
+        options: {
+          prefix: process.env.PREFIX,
+          VIP_ROLE_NAME: null,
+          BASE_ROLE_NAME: null,
+          EXTERNAL_ROLE_NAME: null,
+        },
+        joinedAt: Date.now(),
+      });
+      newGuild.save();
+    });
   },
 };
