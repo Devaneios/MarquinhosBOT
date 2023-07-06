43a23a42804e042ba662547b494a4f8568800276
Tiago Campêlo <tscs2@cin.ufpe.br>
2023-07-06 01:42:38 -0300

    feat: add coin, move-all, esroh

--- src/commands/slashCommands/index.ts
+++ src/commands/slashCommands/index.ts
@@ -2,3 +2,5 @@ export { ping } from './ping';
 export { configRoles } from './configRoles';
 export { getRoles } from './getRoles';
 export { chaos } from './chaos';
+export { moverTodos } from './moverTodos';
+export { moeda } from './moeda';
--- src/commands/textCommands/index.ts
+++ src/commands/textCommands/index.ts
@@ -1,5 +1,6 @@
 export { anom } from './anom';
 export { cavalo } from './cavalo';
+export { olavac } from './olavac';
 export { chaos } from './chaos';
 export { secretChat } from './secretChat';
 export { disconnectAll } from './disconnectAll';
