1a1e8b21c939b857b628d7bb1813283c5a7dc344
Guilherme Afonso <gasp@cin.ufpe.br>
2023-07-08 00:32:40 -0300

    fix: organize and prettier code

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -4,67 +4,61 @@ import { GuildMember, VoiceState } from 'discord.js';
 
 // WIP
 export const voiceStateUpdate: BotEvent = {
-    name: 'voiceStateUpdate',
-    execute: async (oldState: VoiceState, newState: VoiceState) => {
+  name: 'voiceStateUpdate',
+  execute: async (oldState: VoiceState, newState: VoiceState) => {
     const member = oldState.member;
-        if(oldState.channel === null && newState.channel !== null) {
-            userJoinedVoiceChannel(member);
-            return; 
-        } 
+    const oldChannel = oldState.channel;
+    const newChannel = newState.channel;
 
-        if(oldState.channel !== null && newState.channel === null) {
-            userLeftVoiceChannel(member);
-            return;
-        }
-
-        if(oldState.channel !== null && newState.channel !== null && oldState.channelId !== newState.channelId) {
-            userChangedVoiceChannel(member);
-            return;
-        }
-
-        if(oldState.channel !== null && newState.channel !== null && oldState.channelId === newState.channelId) {
-            userChangedVoiceState(member);
-            return;
-        }
+    if (oldChannel && !newChannel) {
+      userJoinedVoiceChannel(member);
+    } else if (!oldChannel && newChannel) {
+      userLeftVoiceChannel(member);
+    } else if (!oldChannel && !newChannel && newChannel.id !== oldChannel.id) {
+      userChangedVoiceChannel(member);
+    } else if (!oldChannel && !newChannel && newChannel.id === oldChannel.id) {
+      userChangedVoiceState(member);
     }
-}
-  
+  },
+};
+
 async function userJoinedVoiceChannel(member: GuildMember) {
-    if(await isUserArrested(member)) {
-        arrestUser(member);
-    }
+  if (await isUserArrested(member)) {
+    arrestUser(member);
+  }
 }
 
-  
 async function userLeftVoiceChannel(member: GuildMember) {
-    return;
+  return;
 }
 
 async function userChangedVoiceChannel(member: GuildMember) {
-    if (await isUserArrested(member)) {
-        arrestUser(member);
-    }
+  if (await isUserArrested(member)) {
+    arrestUser(member);
+  }
 }
 async function userChangedVoiceState(member: GuildMember) {
-    return;
+  return;
 }
 
 async function isUserArrested(member: GuildMember) {
-    return ArrestedModel.collection.findOne({ id: member.id, tag: member.user.tag });
+  return ArrestedModel.collection.findOne({
+    id: member.id,
+    tag: member.user.tag,
+  });
 }
 
 async function arrestUser(member: GuildMember) {
-    if(member.voice?.channel.id != member.guild.afkChannelId){
-        // a try/catch so if the person disconnect, Marquinhos don't break
-        try {
-            member.voice.setChannel(member.guild.afkChannelId);
-            member.send("Você está preso! :(");
-        } catch (error) {
-            console.log(error);
-        }
-    } else {
-        // User is already in arrested's channel;
-        return;
+  if (member.voice?.channel.id != member.guild.afkChannelId) {
+    // a try/catch so if the person disconnect, Marquinhos don't break
+    try {
+      member.voice.setChannel(member.guild.afkChannelId);
+      member.send('Você está preso! :(');
+    } catch (error) {
+      console.log(error);
     }
-    
+  } else {
+    // User is already in arrested's channel;
+    return;
+  }
 }
