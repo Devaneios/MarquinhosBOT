8539feaab236a7720a3080a58b9480d0103e90b5
Guilherme Afonso <gasp@cin.ufpe.br>
2023-07-08 14:33:02 -0300

    fix: remove check-in text command

--- src/commands/textCommands/index.ts
+++ src/commands/textCommands/index.ts
@@ -6,4 +6,3 @@ export { secretChat } from './secretChat';
 export { disconnectAll } from './disconnectAll';
 export { clone } from './clone';
 export { time } from './time';
-export { checkIn } from './checkIn';
