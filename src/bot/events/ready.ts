47bcbb0a107f1e6d417ad74f783641ebb4d93e90
Guilherme Afonso <gasp@cin.ufpe.br>
2023-09-17 20:25:23 -0300

    feat: add avatar update based on holiday

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,18 +1,21 @@
 import { Client } from 'discord.js';
+import { join } from 'path';
 
 import { BotEvent } from 'src/types';
 import { logger } from 'src/utils/logger';
 import { getBicho } from 'src/utils/bichoGame';
 import GuildModel from 'src/database/schemas/guild';
+import { isDateInRange } from 'src/utils/dateRange';
 
 export const ready: BotEvent = {
   name: 'ready',
   once: true,
-  execute: (client: Client) => {
+  execute: async (client: Client) => {
     logger.info(`Logged in as ${client.user?.tag}`);
     logger.info(`Marquinhos™ is online!`);
     // Start heartbeat for bicho game
     startBichoGame(client);
+    await updateAvatarBasedOnHoliday(client);
     // Marquinhos for Multiple guilds
     const guilds = client.guilds.cache.map((guild) => guild.id);
     guilds.forEach(async (guild) => {
@@ -39,9 +42,48 @@ export const ready: BotEvent = {
   },
 };
 
-async function startBichoGame(client: Client) {
+function startBichoGame(client: Client) {
   client.user.setActivity(getBicho());
   setInterval(function () {
     client.user.setActivity(getBicho());
   }, 100 * 1000);
+}
+
+async function updateAvatarBasedOnHoliday(client: Client) {
+  const avatar = getAvatar(client?.user.avatar);
+  if (!avatar) return;
+  await client.user.setAvatar(
+    join(__dirname, `../../resources/images/${avatar}`)
+  );
+}
+
+function getAvatar(avatarHash: string | null) {
+  const regularAvatar = 'marquinhoshead.jpg';
+  const regularAvatarHash = '3b124d750ce2a473031e559993179653';
+
+  const christmasAvatar = 'marquinhosnatal.png';
+  const christmasAvatarHash = 'b4b215d7e3176ce99b73097c7f3d5985';
+  const christmasStartDate = '27/11';
+  const christmasEndDate = '06/01';
+
+  const halloweenAvatar = 'marquinhos_halloween.png';
+  const halloweenAvatarHash = '40aab1d039453c791474e772c18bb6a0';
+  const halloweenStartDate = '24/10';
+  const halloweenEndDate = '06/11';
+
+  if (isDateInRange(halloweenStartDate, halloweenEndDate)) {
+    if (avatarHash === halloweenAvatarHash) {
+      return null;
+    }
+    return halloweenAvatar;
+  } else if (isDateInRange(christmasStartDate, christmasEndDate)) {
+    if (avatarHash === christmasAvatarHash) {
+      return null;
+    }
+    return christmasAvatar;
+  }
+  if (avatarHash === regularAvatarHash) {
+    return null;
+  }
+  return regularAvatar;
 }
