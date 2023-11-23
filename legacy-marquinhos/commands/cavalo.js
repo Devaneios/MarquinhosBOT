b3dc5f91e3f75c5fa821a8e40564c6830d1efa26
Guilherme Afonso <gasp@cin.ufpe.br>
2023-11-22 21:45:01 -0300

    refactor: remove legacy code

--- legacy-marquinhos/commands/cavalo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "cavalo",
-    description: "CAVALO?",
-    usage: "!cavalo",
-    execute(message, args) {
-        message.channel.send({ files: ["./resources/animations/cavalo.gif"] });
-    },
-};
--- legacy-marquinhos/commands/olavac.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "olavac",
-    description: "!oirártnoC",
-    usage: "!olavac",
-    execute(message, args) {
-        message.channel.send({files: ["./resources/animations/olavac.gif"]});
-    },
-};
