6f799d5e35ae0e81e2e8f1b2e1b6e39a23379ab1
Guilherme Afonso <gasp@cin.ufpe.br>
2023-11-22 21:45:01 -0300

    refactor: remove legacy code

--- legacy-marquinhos/commands/dm.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "dm",
-    description: "Te envio uma mensagem ;D",
-    usage: "!dm",
-    execute(message, args) {
-        console.log("Sending dm to " + message.author.username);
-        message.author.send("Dale menó");
-    },
-};
--- legacy-marquinhos/events/guildMemberRemove.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-module.exports = async (client, member) => {
-    member.guild.channels.cache
-        .get(process.env.DEFAULT_CHANNEL_ID)
-        .send(member.user.username + " fechou sua diária!");
-};
