7d76ef3cba006663f8db329e39ebee2b806c8d3f
Guilherme Afonso <guilhermeafonso208@gmail.com>
2024-03-31 18:57:02 -0300

    refactor: organize code and use base embed from client

--- src/bot/commands/slashCommands/allowUser.ts
+++ src/bot/commands/slashCommands/allowUser.ts
@@ -1,11 +1,19 @@
 import {
   SlashCommandBuilder,
   EmbedBuilder,
-  GuildMemberRoleManager,
+  GuildMember,
+  PermissionFlagsBits,
+  CommandInteraction,
 } from 'discord.js';
 
-import { Nullable, SlashCommand } from '@marquinhos/types';
+import { Nullable, RolesConfig, SlashCommand } from '@marquinhos/types';
 import GuildModel from '@schemas/guild';
+
+const roleTranslations = {
+  externalRoleId: 'Cargo externo',
+  baseRoleId: 'Cargo base',
+  vipRoleId: 'Cargo vip',
+};
 
 export const allowUser: SlashCommand = {
   command: new SlashCommandBuilder()
@@ -20,117 +28,170 @@ export const allowUser: SlashCommand = {
   execute: async (interaction) => {
     const userId = interaction.options.get('usuário')
       ?.value as Nullable<string>;
-    const roles = {
-      externalRoleId: null,
-      baseRoleId: null,
-      vipRoleId: null,
-    } as {
-      externalRoleId: Nullable<string>;
-      baseRoleId: Nullable<string>;
-      vipRoleId: Nullable<string>;
-    };
+    const roles = (await findRoles(
+      (interaction.member as GuildMember).guild.id
+    )) as RolesConfig;
+    const allowUserEmbed = interaction.client.baseEmbed();
+    const selectedMember = (await interaction.guild?.members.fetch(
+      userId ?? ''
+    )) as GuildMember;
+
+    await validateRoles(roles, interaction, allowUserEmbed);
+    await validatePermissions(
+      roles.vipRoleId as string,
+      interaction,
+      allowUserEmbed
+    );
+    await validateMemberExists(selectedMember, interaction, allowUserEmbed);
+    await validateMemberHasAccess(
+      selectedMember,
+      roles,
+      interaction,
+      allowUserEmbed
+    );
 
     try {
-      const guildRoles = await findRoles(
-        interaction.guild?.id ?? ('' as string)
-      );
-
-      roles.externalRoleId = guildRoles.externalRoleId;
-      roles.baseRoleId = guildRoles.baseRoleId;
-      roles.vipRoleId = guildRoles.vipRoleId;
-
-      if (!roles.externalRoleId || !roles.baseRoleId || !roles.vipRoleId) {
-        throw new Error('Roles not configured');
-      }
-    } catch (error: unknown) {
-      await interaction.reply({
+      await interaction.deferReply();
+      await interaction.followUp({
         embeds: [
-          new EmbedBuilder()
+          allowUserEmbed
+            .setThumbnail(selectedMember.user.displayAvatarURL())
+            .setDescription(`Liberando o usuário <@${userId}>...`),
+        ],
+      });
+      await selectedMember.roles.remove(roles.externalRoleId as string);
+      await selectedMember.roles.add(roles.baseRoleId as string);
+      await interaction.editReply({
+        embeds: [
+          allowUserEmbed
+            .setTitle('Usuário liberado')
             .setDescription(
-              'Cargos não configurados. Peça a um admin para usar o comando `/config-roles` para configurar os cargos.'
+              `O usuário <@${userId}> foi liberado para acessar o servidor.`
+            ),
+        ],
+      });
+    } catch (error) {
+      await interaction.editReply({
+        embeds: [
+          allowUserEmbed
+            .setTitle('Erro ao executar o comando')
+            .setDescription(
+              'Houve um erro ao liberar o usuário.\n\nTente novamente ou entre em contato com um admin.'
             )
             .setColor('#ff0000'),
         ],
       });
       throw new Error((error as Error).message);
     }
-
-    if (
-      !(interaction.member?.roles as GuildMemberRoleManager).cache.has(
-        roles.vipRoleId
-      )
-    ) {
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription('Você não tem permissão para usar esse comando.')
-            .setColor('#ff0000'),
-        ],
-      });
-      throw new Error('User not allowed');
-    }
-
-    const member = await interaction.guild?.members.fetch(userId ?? '');
-
-    if (!member) {
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription('Usuário não encontrado.')
-            .setColor('#ff0000'),
-        ],
-      });
-      throw new Error('User not found');
-    }
-
-    const memberRoles = member.roles.cache;
-
-    try {
-      if (memberRoles.has(roles.externalRoleId)) {
-        await member.roles.remove(roles.externalRoleId);
-      } else if (memberRoles.has(roles.baseRoleId)) {
-        await interaction.reply({
-          embeds: [
-            new EmbedBuilder()
-              .setDescription('O usuário já tem acesso ao servidor.')
-              .setColor('#ff0000'),
-          ],
-        });
-        throw new Error('User already allowed');
-      }
-
-      if (!memberRoles.has(roles.baseRoleId)) {
-        await member.roles.add(roles.baseRoleId);
-      }
-    } catch (error) {
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription('Erro ao liberar o usuário.')
-            .setColor('#ff0000'),
-        ],
-      });
-      throw new Error((error as Error).message);
-    }
-
-    await interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `O usuário <@${userId}> foi liberado para acessar o servidor.`
-          )
-          .setColor('#00ff00'),
-      ],
-    });
   },
   cooldown: 10,
 };
 
-async function findRoles(guildId: string) {
+async function findRoles(guildId: string): Promise<RolesConfig> {
   const guild = await GuildModel.findOne({ guildID: guildId }).exec();
   if (guild) {
     const { externalRoleId, baseRoleId, vipRoleId } = guild.options;
     return { externalRoleId, baseRoleId, vipRoleId };
   }
   return { externalRoleId: null, baseRoleId: null, vipRoleId: null };
+}
+
+function userHasPermission(member: GuildMember, vipRoleId: string): boolean {
+  return (
+    member.roles.cache.has(vipRoleId) ||
+    member.permissions.has(PermissionFlagsBits.Administrator)
+  );
+}
+
+async function validateRoles(
+  roles: RolesConfig,
+  interaction: CommandInteraction,
+  allowUserEmbed: EmbedBuilder
+) {
+  let rolesNotConfiguredList = 'Os seguintes cargos não estão configurados: \n';
+
+  for (const role in roles) {
+    if (!roles[role as keyof RolesConfig]) {
+      rolesNotConfiguredList += `- ${
+        roleTranslations[role as keyof RolesConfig]
+      }\n`;
+    }
+  }
+
+  if (!roles.externalRoleId || !roles.baseRoleId || !roles.vipRoleId) {
+    await interaction.reply({
+      embeds: [
+        allowUserEmbed
+          .setTitle('Erro executar o comando')
+          .setDescription(
+            `${rolesNotConfiguredList}
+Peça a um admin para usar o comando \` /configurar-cargos\` para configurar os cargos.`
+          )
+          .setColor('#ff0000'),
+      ],
+      ephemeral: true,
+    });
+    throw new Error('Roles not configured');
+  }
+}
+
+async function validatePermissions(
+  vipRoleId: string,
+  interaction: CommandInteraction,
+  allowUserEmbed: EmbedBuilder
+) {
+  if (!userHasPermission(interaction.member as GuildMember, vipRoleId)) {
+    await interaction.reply({
+      embeds: [
+        allowUserEmbed
+          .setTitle('Erro ao executar o comando')
+          .setDescription('Você não tem permissão para usar esse comando.')
+          .setColor('#ff0000'),
+      ],
+      ephemeral: true,
+    });
+    throw new Error('User not allowed');
+  }
+}
+
+async function validateMemberExists(
+  selectedMember: GuildMember,
+  interaction: CommandInteraction,
+  allowUserEmbed: EmbedBuilder
+) {
+  if (!selectedMember) {
+    await interaction.reply({
+      embeds: [
+        allowUserEmbed
+          .setTitle('Erro ao executar o comando')
+          .setDescription('Usuário não encontrado.')
+          .setColor('#ff0000'),
+      ],
+      ephemeral: true,
+    });
+    throw new Error('User not found');
+  }
+}
+
+async function validateMemberHasAccess(
+  selectedMember: GuildMember,
+  roles: RolesConfig,
+  interaction: CommandInteraction,
+  allowUserEmbed: EmbedBuilder
+) {
+  if (
+    !selectedMember.roles.cache.has(roles.externalRoleId as string) &&
+    selectedMember.roles.cache.has(roles.baseRoleId as string)
+  ) {
+    await interaction.reply({
+      embeds: [
+        allowUserEmbed
+          .setTitle('Erro ao executar o comando')
+          .setDescription('Usuário já tem acesso ao servidor.')
+          .setColor('#ff0000'),
+      ],
+      ephemeral: true,
+    });
+    throw new Error('User already allowed');
+  }
 }
