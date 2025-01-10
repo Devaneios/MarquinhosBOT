20fad7c9e3bc1057a104eed8017bb59b05696e04
Guilherme Afonso <guilhermeafonso208@gmail.com>
2025-01-10 18:54:56 -0300

    feat: remove message create

--- src/bot/events/index.ts
+++ src/bot/events/index.ts
@@ -1,6 +1,5 @@
 export { guildCreate } from './guildCreate';
 export { guildMemberAdd } from './guildMemberAdd';
 export { interactionCreate } from './interactionCreate';
-export { messageCreate } from './messageCreate';
 export { ready } from './ready';
 export { voiceStateUpdate } from './voiceStateUpdate';
