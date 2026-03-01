ba8091238bc61382f7feb75db82fe0c951754f6a
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-03-01 16:43:29 -0300

    refactor: remove command registration from startup process

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { config } from 'dotenv';
 import { MarquinhosBot } from '@marquinhos/bot/marquinhos-bot';
-import { registerCommands } from './register-slash-commands';
 import { safeExecute } from './utils/errorHandling';
 
 process.env.ROOT_DIR = __dirname;
 config();
-
-await registerCommands();
 
 const marquinhos = new MarquinhosBot();
 
