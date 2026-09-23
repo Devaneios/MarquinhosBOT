20f6fec2094b06878b8d410677a7d820744a1f58
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-09-23 14:05:58 -0300

    build(workspace): add shared tsconfig and eslint-config packages
    Foundation for the new packages/* workspace split: a shared base
    tsconfig and eslint rule set that the domain/contracts/database/
    api-client packages and the apps will consume.

---
+++ packages/eslint-config/rules.js
@@ -0,0 +1,32 @@
+export const rules = {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+
+      'import/order': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-duplicates': 'off',
+
+      'no-console': 'off',
+      'no-debugger': 'error',
+      'no-duplicate-imports': 'off',
+      'no-unused-vars': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+      eqeqeq: ['error', 'always'],
+};
