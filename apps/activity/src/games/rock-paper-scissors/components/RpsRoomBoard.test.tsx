a2784545c407aee1c39f3ea6c1410d573ba9ed35
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-09-24 02:33:25 -0300

    test: add i18n import to RpsRoomBoard test file

--- apps/activity/src/games/rock-paper-scissors/components/RpsRoomBoard.test.tsx
+++ apps/activity/src/games/rock-paper-scissors/components/RpsRoomBoard.test.tsx
@@ -1,5 +1,6 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it, mock } from 'bun:test';
+import '../../../i18n/index';
 import { RoomConnectionContext } from '../../../realtime/RoomConnectionProvider';
 import { RpsRoomBoard } from './RpsRoomBoard';
 
