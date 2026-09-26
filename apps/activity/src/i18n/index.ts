d28a0fd3b4e715f115a60750f2614b2fb29d9fde
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-09-25 21:48:50 -0300

    refactor(activity): group per-game locale files under locales/pt-BR/games

--- apps/activity/src/i18n/index.ts
+++ apps/activity/src/i18n/index.ts
@@ -1,27 +1,27 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import battleship from './locales/pt-BR/battleship.json';
-import bingoSpeed from './locales/pt-BR/bingo-speed.json';
-import boggleWordRace from './locales/pt-BR/boggle-word-race.json';
-import cards from './locales/pt-BR/cards.json';
-import checkers from './locales/pt-BR/checkers.json';
 import common from './locales/pt-BR/common.json';
-import connectFour from './locales/pt-BR/connect-four.json';
-import dominoesBlock from './locales/pt-BR/dominoes-block.json';
 import games from './locales/pt-BR/games.json';
-import hangman from './locales/pt-BR/hangman.json';
-import minesweeperVersus from './locales/pt-BR/minesweeper-versus.json';
-import pong from './locales/pt-BR/pong.json';
-import rockPaperScissors from './locales/pt-BR/rock-paper-scissors.json';
+import battleship from './locales/pt-BR/games/battleship.json';
+import bingoSpeed from './locales/pt-BR/games/bingo-speed.json';
+import boggleWordRace from './locales/pt-BR/games/boggle-word-race.json';
+import cards from './locales/pt-BR/games/cards.json';
+import checkers from './locales/pt-BR/games/checkers.json';
+import connectFour from './locales/pt-BR/games/connect-four.json';
+import dominoesBlock from './locales/pt-BR/games/dominoes-block.json';
+import hangman from './locales/pt-BR/games/hangman.json';
+import minesweeperVersus from './locales/pt-BR/games/minesweeper-versus.json';
+import pong from './locales/pt-BR/games/pong.json';
+import rockPaperScissors from './locales/pt-BR/games/rock-paper-scissors.json';
+import snakeGame from './locales/pt-BR/games/snake-game.json';
+import ticTacToe from './locales/pt-BR/games/tic-tac-toe.json';
+import towerUnstable from './locales/pt-BR/games/tower-unstable.json';
+import triviaQuiz from './locales/pt-BR/games/trivia-quiz.json';
+import wordChain from './locales/pt-BR/games/word-chain.json';
+import wordSearchRace from './locales/pt-BR/games/word-search-race.json';
+import wordleRace from './locales/pt-BR/games/wordle-race.json';
+import wordle from './locales/pt-BR/games/wordle.json';
 import rooms from './locales/pt-BR/rooms.json';
-import snakeGame from './locales/pt-BR/snake-game.json';
-import ticTacToe from './locales/pt-BR/tic-tac-toe.json';
-import towerUnstable from './locales/pt-BR/tower-unstable.json';
-import triviaQuiz from './locales/pt-BR/trivia-quiz.json';
-import wordChain from './locales/pt-BR/word-chain.json';
-import wordSearchRace from './locales/pt-BR/word-search-race.json';
-import wordleRace from './locales/pt-BR/wordle-race.json';
-import wordle from './locales/pt-BR/wordle.json';
 
 void i18next.use(initReactI18next).init({
   lng: 'pt-BR',
